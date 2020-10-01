6ef8ca1491f077bfe08dca2bbbfcd3685cb0995e
Fa3ris <Fa3ris@users.noreply.github.com>
2020-10-01 22:12:28 +0200

    conf
    prettier
    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,20 @@
-var HtmlWebpackPlugin = require("html-webpack-plugin");
-var path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require("path");
 
 module.exports = {
+  mode: "development",
   entry: "./src/main.js",
-  watch: true,
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
+  },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: "./index.html",
     }),
